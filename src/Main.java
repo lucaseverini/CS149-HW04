a70b1c05b3a515d58fc81d0a59e0cf46f2668a2f
Luca Severini <lucaseverini@mac.com>
2014-03-21 12:56:07 -0700

    Implemented Paging class. Cleaned up main class.

--- src/Main.java
+++ src/Main.java
@@ -13,151 +13,25 @@ public class Main {
     public static final int PROCESSES = 100;
     public static final int QUANTA = 60;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+	{
         printToFile printer = new printToFile();
         String simulationString = "";
         String totalFile = "";
         String swapping;
-//        String RR;
-//        String FCFS;
-//        String SJF;
-//        String SRT;
 
-//        FCFS = runFCFS();
-//        totalFile += FCFS;
-//       
-        swapping = runSwapping(1); //run first fit
+		swapping = runSwapping(1); //run first fit
         totalFile += swapping;
         swapping = runSwapping(2); //run next fit
         totalFile += swapping;
         swapping = runSwapping(3); //run best fit
         totalFile += swapping;
+		
+		Paging paging = new Paging();
+		paging.runPaging(SIMULATIONS, PROCESSES);
 
-//        RR = runRR();
-//        totalFile += RR;
-//
-//        SJF = runSJF();
-//        totalFile += SJF;
-//        SRT = runSRT();
-//        totalFile += SRT;
-        printer.printToFile(totalFile);
+		printer.printToFile(totalFile);
     }
-
-//
-//    public static String runFCFS() {
-//        ArrayList<Process> processArrayList;
-//        ArrayList<Process> unsortedArrayList;
-//        String simulationString;
-//        String totalFile = "";
-//        float[] statistics;
-//        float averageWaitingTime = 0;
-//        float averageTurnaroundTime = 0;
-//        float averageResponseTime = 0;
-//        float throughput = 0;
-//        float totalAverageWaitingTime;
-//        float totalAverageTurnaroundTime;
-//        float totalAverageResponseTime;
-//        float totalThroughput;
-//        int i;
-//
-//        // ProcessGenerator: first parameter is number of processes to generate
-//        // second parameter is seed number for random function.
-//        ProcessGenerator newProcesses;
-//
-//        FirstComeFirstServed FCFS;
-//
-//        //Running 5 simulations, and adding results to 'totalFile' to be printed out
-//        for (i = 0; i < SIMULATIONS; i++) {
-//            newProcesses = new ProcessGenerator(PROCESSES, i);
-//            processArrayList = newProcesses.generateProcesses();
-//            unsortedArrayList = newProcesses.getUnsortedArrayList();
-//
-//            FCFS = new FirstComeFirstServed(processArrayList, unsortedArrayList);
-//            simulationString = FCFS.simulateFCFS();
-//
-//            statistics = FCFS.getStatistics();
-//            if (i == 0) {
-//                totalFile += "---------------------------------------------------------------------------------------";
-//            }
-//            totalFile += "\nSimulation #" + (i + 1) + " of First Come First Served: \n";
-//            totalFile += simulationString;
-//
-//            averageWaitingTime += statistics[0];
-//            averageResponseTime += statistics[2];
-//            averageTurnaroundTime += statistics[1];
-//            throughput += statistics[3];
-//        }
-//
-//        totalAverageWaitingTime = averageWaitingTime / i;
-//        totalAverageResponseTime = averageResponseTime / i;
-//        totalAverageTurnaroundTime = averageTurnaroundTime / i;
-//        totalThroughput = throughput / (float) i;
-//
-//        totalFile += "\nTotal Average Waiting Time for FCFS was: " + totalAverageWaitingTime;
-//        totalFile += "\nTotal Average Response Time for FCFS was: " + totalAverageResponseTime;
-//        totalFile += "\nTotal Average Turnaround for FCFS was: " + totalAverageTurnaroundTime;
-//        totalFile += "\nTotal Average Throughput for FCFS was: " + totalThroughput + "\n\n";
-//
-//        return totalFile;
-//    }
-//    public static String runRR() {
-//        ArrayList<Process> processArrayList;
-//        ArrayList<Process> unsortedArrayList;
-//
-//        String simulationString;
-//        String totalFile = "";
-//        float[] statistics;
-//        float averageWaitingTime = 0;
-//        float averageTurnaroundTime = 0;
-//        float averageResponseTime = 0;
-//        float throughput = 0;
-//        float totalAverageWaitingTime;
-//        float totalAverageTurnaroundTime;
-//        float totalAverageResponseTime;
-//        float totalThroughput;
-//        int i;
-//
-//        // ProcessGenerator: first parameter is number of processes to generate
-//        // second parameter is seed number for random function.
-//        ProcessGenerator newProcesses;
-//
-//        RoundRobin RR;
-//
-//        //Running 5 simulations, and adding results to 'totalFile' to be printed out
-//        for (i = 0; i < SIMULATIONS; i++) {
-//            newProcesses = new ProcessGenerator(PROCESSES, i);
-//            processArrayList = newProcesses.generateProcesses();
-//            unsortedArrayList = newProcesses.getUnsortedArrayList();
-//
-//            RR = new RoundRobin(processArrayList, unsortedArrayList);
-//            simulationString = RR.simulateRR();
-//            statistics = RR.getStatistics();
-//            if (i == 0) {
-//                totalFile += "---------------------------------------------------------------------------------------";
-//
-//            }
-//            totalFile += "\nSimulation #" + (i + 1) + " of Round Robin: \n";
-//            totalFile += simulationString;
-//
-//            averageWaitingTime += statistics[0];
-//            averageResponseTime += statistics[2];
-//            averageTurnaroundTime += statistics[1];
-//            throughput += statistics[3];
-//        }
-//
-//        totalAverageWaitingTime = averageWaitingTime / i;
-//        totalAverageResponseTime = averageResponseTime / i;
-//        totalAverageTurnaroundTime = averageTurnaroundTime / i;
-//        totalThroughput = throughput / (float) i;
-//
-//        totalFile += "\nTotal Average Waiting Time for RR was: " + totalAverageWaitingTime;
-//        totalFile += "\nTotal Average Response Time for RR was: " + totalAverageResponseTime;
-//        totalFile += "\nTotal Average Turnaround for RR was: " + totalAverageTurnaroundTime;
-//        totalFile += "\nTotal Average Throughput for RR was: " + totalThroughput + "\n\n";
-//
-//        return totalFile;
-//    }
-    
     
     /**
      * This simulates the swapping algorithm
@@ -165,7 +39,8 @@ public class Main {
      * @param type the type of simulation being run
      * @return 'oneSimulation' The string representing the simulation
      */
-    public static String runSwapping(int type) {
+    public static String runSwapping(int type) 
+	{
         ArrayList<Process> processArrayList;
         ArrayList<Process> unsortedArrayList;
 
@@ -212,108 +87,4 @@ public class Main {
 
         return totalFile;
     }
-
-//    public static String runSJF() {
-//        ArrayList<Process> processArrayList;
-//        ArrayList<Process> unsortedArrayList;
-//        String simulationString;
-//        String totalFile = "";
-//        float[] statistics;
-//        float averageWaitingTime = 0;
-//        float averageTurnaroundTime = 0;
-//        float averageResponseTime = 0;
-//        float throughput = 0;
-//        float totalAverageWaitingTime;
-//        float totalAverageTurnaroundTime;
-//        float totalAverageResponseTime;
-//        float totalThroughput;
-//        int i;
-//
-//        // ProcessGenerator: first parameter is number of processes to generate
-//        // second parameter is seed number for random function.
-//        ProcessGenerator newProcesses;
-//
-//        ShortestJobFirst SJF;
-//
-//        //Running 5 simulations, and adding results to 'totalFile' to be printed out
-//        for (i = 0; i < SIMULATIONS; i++) {
-//            newProcesses = new ProcessGenerator(PROCESSES, i);
-//            processArrayList = newProcesses.generateProcesses();
-//            unsortedArrayList = newProcesses.getUnsortedArrayList();
-//
-//            SJF = new ShortestJobFirst(processArrayList, unsortedArrayList);
-//            simulationString = SJF.simulateSJF();
-//            statistics = SJF.getStatistics();
-//            if (i == 0) {
-//                totalFile += "---------------------------------------------------------------------------------------";
-//            }
-//            totalFile += "\nSimulation #" + (i + 1) + " of Shortest Job First: \n";
-//            totalFile += simulationString;
-//
-//            averageWaitingTime += statistics[0];
-//            averageResponseTime += statistics[2];
-//            averageTurnaroundTime += statistics[1];
-//            throughput += statistics[3];
-//        }
-//
-//        totalAverageWaitingTime = averageWaitingTime / i;
-//        totalAverageResponseTime = averageResponseTime / i;
-//        totalAverageTurnaroundTime = averageTurnaroundTime / i;
-//        totalThroughput = throughput / (float) i;
-//
-//        totalFile += "\nTotal Average Waiting Time for SJF was: " + totalAverageWaitingTime;
-//        totalFile += "\nTotal Average Response Time for SJF was: " + totalAverageResponseTime;
-//        totalFile += "\nTotal Average Turnaround for SJF was: " + totalAverageTurnaroundTime;
-//        totalFile += "\nTotal Average Throughput for SJF was: " + totalThroughput + "\n\n";
-//
-//        return totalFile;
-//    }
-//
-//    public static String runSRT() {
-//        ArrayList<Process> processArrayList;
-//        String simulationString;
-//        String totalFile = "";
-//        float[] statistics;
-//        float averageWaitingTime = 0;
-//        float averageTurnaroundTime = 0;
-//        float averageResponseTime = 0;
-//        float throughput = 0;
-//        float totalAverageWaitingTime;
-//        float totalAverageTurnaroundTime;
-//        float totalAverageResponseTime;
-//        float totalThroughput;
-//
-//        for (int idx = 0; idx < SIMULATIONS; idx++) {
-//            ProcessGenerator procGen = new ProcessGenerator(PROCESSES, idx);
-//            processArrayList = procGen.generateProcesses();
-//
-//            ShortestRemainingTime SRT = new ShortestRemainingTime(processArrayList);
-//
-//            simulationString = SRT.simulatePreemptive(QUANTA);
-//
-//            statistics = SRT.getStatistics();
-//            if (idx == 0) {
-//                totalFile += "---------------------------------------------------------------------------------------";
-//            }
-//            totalFile += "\nSimulation #" + (idx + 1) + " of Shortest Remaining  Time (SRT): \n";
-//            totalFile += simulationString;
-//
-//            averageWaitingTime += statistics[0];
-//            averageResponseTime += statistics[2];
-//            averageTurnaroundTime += statistics[1];
-//            throughput += statistics[3];
-//        }
-//
-//        totalAverageWaitingTime = averageWaitingTime / SIMULATIONS;
-//        totalAverageResponseTime = averageResponseTime / SIMULATIONS;
-//        totalAverageTurnaroundTime = averageTurnaroundTime / SIMULATIONS;
-//        totalThroughput = throughput / SIMULATIONS;
-//
-//        totalFile += "\nTotal Average Waiting Time for SRT was: " + totalAverageWaitingTime;
-//        totalFile += "\nTotal Average Response Time for SRT was: " + totalAverageResponseTime;
-//        totalFile += "\nTotal Average Turnaround for SRT was: " + totalAverageTurnaroundTime;
-//        totalFile += "\nTotal Average Throughput for SRT was: " + totalThroughput + "\n\n";
-//
-//        return totalFile;
-//    }
 }
--- src/Paging.java
+++ src/Paging.java
@@ -16,11 +16,21 @@ import java.util.*;
 public class Paging 
 {
 	static int maxPagesInMemory = 4;
-	static int maxPagesOnDisk = 100;
+	static int maxPagesOnDisk = 10;
 	static int numProcessPages = 10;
-	int pagesInMemory;
 	private final ArrayDeque<PageFrame> memoryPages;
 	private final ArrayList<PageFrame> diskPages;
+	private int[] referenceCounter;
+	private int pageFault;
+	
+	public enum TestType 
+	{  
+		FIFO,	// First-In First-Out
+		LRU,	// Least Recently Used
+		LFU,	// Least Frequently Used
+		MFU,	// Most Frequently Used
+		RANDOM	// Random Pick
+	}
 
 	public Paging()
 	{
@@ -28,17 +38,69 @@ public class Paging
 		diskPages = new ArrayList<>(maxPagesOnDisk);
 	}
 	
-	void runPaging()
+	void runPaging(int runCount, int referenceCount)
 	{
 		Process process = new Process();
 		
-		process.runProcess();
+		for(int idx = 0; idx < runCount; idx++)
+		{
+			resetStatistics();	
+			process.run(TestType.FIFO, referenceCount);
+			printStatistics();
+		}
+		
+		for(int idx = 0; idx < runCount; idx++)
+		{
+			resetStatistics();	
+			process.run(TestType.LRU, referenceCount);
+			printStatistics();
+		}
+		
+		for(int idx = 0; idx < runCount; idx++)
+		{
+			resetStatistics();	
+			process.run(TestType.LFU, referenceCount);
+			printStatistics();
+		}
+		
+		for(int idx = 0; idx < runCount; idx++)
+		{
+			resetStatistics();	
+			process.run(TestType.MFU, referenceCount);
+			printStatistics();
+		}
+	
+		for(int idx = 0; idx < runCount; idx++)
+		{
+			resetStatistics();	
+			process.run(TestType.RANDOM, referenceCount);
+			printStatistics();
+		}
+	}
+	
+	void resetStatistics()
+	{
+		referenceCounter = new int[maxPagesOnDisk];
+		
+		pageFault = 0;
+	}
+	
+	void printStatistics()
+	{
+		System.out.printf("PageFaults: %d\n", pageFault);
+			
+		System.out.printf("Page - References\n");
+		for(int idx = 0; idx < maxPagesOnDisk; idx++)
+		{
+			System.out.printf("%-4d   %d\n", idx, referenceCounter[idx]);
+		}
 	}
 	
 	private class PageFrame
 	{
 		public int id;
 		public int referencedCount;
+		public long lastReferenceTime;
 		public boolean onDisk;
 		public boolean inMemory;
 		
@@ -50,36 +112,44 @@ public class Paging
 	private class Process
 	{
 		private final Random rand;
-		int pageReference = 5;
+		int pageReference = 0;
 		private ArrayList<PageFrame> processPages;
+		private TestType test;
 	
 		public Process()
 		{
 			rand = new Random();
 			
-			loadProcessOnDisk();
+			initPageTable();
 		}
 	
-		public void runProcess()
+		public void run(TestType testType, int referenceCount)
 		{
-			for(int idx = 0; idx < 100; idx++)
+			test = testType;
+			
+			System.out.printf("\nRunning process with %s paging for %d references\n", testType, referenceCount);
+			
+			for(int idx = 0; idx < referenceCount; idx++)
 			{
 				pageReference = generatePageReference();
 				
 				PageFrame page = processPages.get(pageReference);
 				
 				page.referencedCount++;
+				page.lastReferenceTime = System.currentTimeMillis();
 				
-				System.out.printf("\nPage %d referenced (%d)\n", page.id, page.referencedCount);
+				// System.out.printf("\nPage %d referenced (%d)\n", page.id, page.referencedCount);
 				
 				if(!page.inMemory)
 				{
-					loadPageInMemory(page);
+					pageFault++;
+					
+					loadPageToMemory(page);
 				}
 			}
 		}
 		
-		private void loadProcessOnDisk()
+		private void initPageTable()
 		{
 			processPages = new ArrayList<>();
 			
@@ -90,16 +160,16 @@ public class Paging
 				
 				processPages.add(page);
 				
-				page.onDisk = true;
+				page.onDisk = true;			// at beginning every page is on disk...
 				page.inMemory = false;
 				
 				diskPages.add(page);
 				
-				System.out.printf("Page %d loaded on disk\n", page.id);
+				// System.out.printf("Page %d loaded on disk\n", page.id);
 			}
 		}
 		
-		private void loadPageInMemory(PageFrame page)
+		private void loadPageToMemory(PageFrame page)
 		{
 			if(memoryPages.size() == maxPagesInMemory)
 			{
@@ -110,40 +180,113 @@ public class Paging
 			page.inMemory = true;
 			page.onDisk = false;
 			
-			System.out.printf("Page %d loaded in memory\n", page.id);
+			// System.out.printf("Page %d loaded in memory\n", page.id);
 		}
 	
 		private void unloadPageFromMemory()
 		{
-			PageFrame topPage = memoryPages.pop();
+			PageFrame pageToEvict = null;
+			
+			switch(test)
+			{
+				case FIFO:
+					pageToEvict = memoryPages.pop();
+					break;
+					
+				case LRU:
+					long oldestTime = 0;
+					for(PageFrame p : memoryPages)
+					{
+						if(p.lastReferenceTime > oldestTime)
+						{
+							oldestTime = p.lastReferenceTime;
+							pageToEvict = p;
+						}
+					}
+					memoryPages.remove(pageToEvict);
+					break;
 
-			diskPages.set(topPage.id, topPage);
-			topPage.onDisk = true;
-			topPage.inMemory = false;
+				case LFU:
+					int smallestCount = Integer.MAX_VALUE;
+					for(PageFrame p : memoryPages)
+					{
+						if(p.referencedCount < smallestCount)
+						{
+							smallestCount = p.referencedCount;
+							pageToEvict = p;
+						}
+					}
+					memoryPages.remove(pageToEvict);
+					break;
+					
+				case MFU:
+					int largestCount = 0;
+					for(PageFrame p : memoryPages)
+					{
+						if(p.referencedCount > largestCount)
+						{
+							largestCount = p.referencedCount;
+							pageToEvict = p;
+						}
+					}
+					memoryPages.remove(pageToEvict);
+					break;
+					
+				case RANDOM:
+					Iterator<PageFrame> iter = memoryPages.iterator();
+					for(int idx = rand.nextInt(memoryPages.size()); idx >= 0; idx--)
+					{
+						pageToEvict = iter.next();
+					}
+					memoryPages.remove(pageToEvict);
+					break;				
+			}
+			
+			if(pageToEvict != null)
+			{
+				diskPages.set(pageToEvict.id, pageToEvict);
+				pageToEvict.onDisk = true;
+				pageToEvict.inMemory = false;
 
-			System.out.printf("Page %d unloaded from memory\n", topPage.id);
+				System.out.printf("Page %d unloaded from memory\n", pageToEvict.id);
+			}
+			else
+			{
+				System.out.printf("No Page unloaded from memory\n");
+			}
 		}
 
 		private int generatePageReference()
 		{
-			if((pageReference > 0 && pageReference < (numProcessPages - 1)) && rand.nextInt(10) < 4)
+			if(rand.nextInt(9) < 7)
 			{
-				return pageReference + (rand.nextInt(2) - 1);
+				int pageOffset;
+				
+				do
+				{
+					pageOffset = (rand.nextInt(2) - 1);
+				}
+				while((pageReference + pageOffset < 0) || (pageReference + pageOffset > 9));
+				
+				pageReference += pageOffset;
 			}
 			else
 			{
-				return pageReference = rand.nextInt(numProcessPages);
+				pageReference = rand.nextInt(numProcessPages);
 			}
-		}
-		
+	
+			referenceCounter[pageReference] = referenceCounter[pageReference] + 1;
+
+			return pageReference;
+		}	
 	}
 
 	// For testing only
 	public static void main(String[] args) 
 	{
 		Paging paging = new Paging();
-		
-		paging.runPaging();
+
+		paging.runPaging(5, 100);
 	}
 }
 
